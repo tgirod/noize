9694b7d6373e4affecd27beec05b50c99b669bc9
Tomtom <tomtom@herbesfolles.org>
2023-12-02 15:06:22 +0100

    testing delay line in main.zig
    this should go in a proper test

--- src/main.zig
+++ src/main.zig
@@ -4,23 +4,20 @@ const n = @import("noize.zig");
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allo = gpa.allocator();
-    try n.init(allo, 48000, 2, 1);
+    try n.init(allo, 48000, 2, 2);
     defer n.deinit();
 
-    var root = try n.seq(
-        n.par(
-            n.sin(),
-            n.sin(),
-        ),
-        n.add(),
+    var root = try n.par(
+        n.sin(),
+        n.seq(n.sin(), n.delay(100)),
     );
     defer root.deinit();
 
     n.in[0] = 100; // freq osc 1
-    n.in[1] = 200;
+    n.in[1] = 100;
 
     for (0..480) |index| {
         root.eval(@as(u64, index), n.in, n.out);
-        std.debug.print("{any}\n", .{n.out[0]});
+        std.debug.print("{any} {any}\n", .{ n.out[0], n.out[1] });
     }
 }
