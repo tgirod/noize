c741d45f975f9135f8384577b6b77156000723d9
Tomtom <tomtom@herbesfolles.org>
2023-12-13 15:12:44 +0100

    cleaning

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,14 @@
 const std = @import("std");
 const n = @import("noize.zig");
 
+const step = @as(f32, 1.0 / 48000.0);
+
 pub fn main() !void {
     const Node = n.Sin();
     var node = Node{};
 
     for (0..480) |i| {
-        const out = node.eval(.{@as(f64, @floatFromInt(i)) * 10});
+        const out = node.eval(step, .{@as(f64, @floatFromInt(i)) * 10});
         std.debug.print("{any}\n", .{out[0]});
     }
 }
