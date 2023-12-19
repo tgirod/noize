fb0f0fcb39920b85a24665120297714ede13fd93
Tomtom <tomtom@herbesfolles.org>
2023-12-19 11:35:58 +0100

    variable delay

--- src/main.zig
+++ src/main.zig
@@ -1,19 +1,10 @@
 const std = @import("std");
-const n = @import("noize.zig");
 const jack = @import("jack.zig");
 
+const n = @import("noize.zig");
+
 var client: jack.Client = undefined;
-var out: jack.Port = undefined;
 var step: f32 = undefined;
-
-const Node = n.SeqN(&[_]type{
-    n.Dup(n.Sin(), 2),
-    // n.Add(f32),
-});
-
-var node = Node{};
-
-var now: f32 = 0;
 
 pub fn main() !void {
     client = try jack.Client.init(std.heap.page_allocator, "noize");
@@ -23,27 +14,43 @@ pub fn main() !void {
 
     try client.setProcessCallback(&processCallback);
 
-    try client.registerPort("out0", .AudioOutput);
-    try client.registerPort("out1", .AudioOutput);
+    try client.registerPort("in", .AudioInput);
+    try client.registerPort("out", .AudioOutput);
     try client.activate();
     defer client.deactivate() catch {};
 
     try client.connect();
 
-    std.debug.print("sleeping\n", .{});
-    std.time.sleep(5 * std.time.ns_per_s);
-    std.debug.print("\nwaking up\n", .{});
+    while (true) {
+        std.time.sleep(std.time.ns_per_s);
+    }
 }
+
+const Lfo = n.SeqN(&[_]type{
+    n.Const(f32, 0.2),
+    n.Sin(),
+    n.MulAdd(f32, 20000, 20000),
+    n.FloatToInt(f32, usize),
+});
+
+const VarDelay = n.Seq(
+    n.Par(n.Id(f32), Lfo),
+    n.Delay(f32, 48000),
+);
+
+var node = n.Rec(
+    n.Mix(f32, 0.3),
+    VarDelay,
+){};
 
 fn processCallback(nframes: u32, arg: ?*anyopaque) callconv(.C) c_int {
     _ = arg;
-    const buf0 = client.outputBuffer(0, nframes);
-    const buf1 = client.outputBuffer(1, nframes);
-    for (buf0, buf1) |*b0, *b1| {
-        const frame = node.eval(step, .{ 400 + now * 10, 400 + now * 15 });
-        b0.* = frame[0];
-        b1.* = frame[1];
-        now += step;
+    const in_buf = client.inputBuffer(0, nframes);
+    const out_buf = client.outputBuffer(0, nframes);
+
+    for (in_buf, out_buf) |*i, *o| {
+        const out = node.eval(step, .{i.*});
+        o.* = out[0];
     }
     return 0;
 }
