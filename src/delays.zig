f067d972f41d175f4f5cb67bd09ba20a68055c6b
Tomtom <tomtom@herbesfolles.org>
2024-02-17 15:47:28 +0100

    MultiDelay

--- src/delays.zig
+++ src/delays.zig
@@ -102,3 +102,53 @@ test "Delay length increasing" {
         try ee(exp, out[0]);
     }
 }
+
+pub fn MultiDelay(comptime srate: f32, comptime maxLength: f32, comptime taps: usize) type {
+    if (maxLength <= 0) {
+        @compileError("maxLength <= 0");
+    }
+
+    if (taps == 0) {
+        @compileError("taps == 0");
+    }
+
+    const size: usize = @intFromFloat(srate * maxLength);
+
+    return struct {
+        pub const in = taps + 1;
+        pub const out = taps;
+
+        mem: [size]f32 = [1]f32{0} ** size,
+        pos: usize = 0,
+
+        pub inline fn eval(self: *@This(), input: [in]f32) [out]f32 {
+            // write input into the memory
+            self.mem[self.pos] = input[0];
+
+            var read: [taps]f32 = undefined;
+            inline for (0..taps) |i| {
+                // compute delay length
+                const length: i64 = @intFromFloat(input[i + 1] * srate);
+                const delay: usize = @intCast(@mod(@as(i64, @intCast(self.pos)) - length, size));
+                // read `delay` samples before current position
+                read[i] = self.mem[delay];
+            }
+            // move position forward
+            self.pos = (self.pos + 1) % size;
+            return read;
+        }
+    };
+}
+
+test "MultiDelay" {
+    const srate: f32 = 48000;
+    const N = MultiDelay(srate, 1.0, 2);
+    try ee(3, N.in);
+    try ee(2, N.out);
+    var n = N{};
+    const input = [_]f32{ 1, 2, 3, 4, 5 };
+    for (input) |in| {
+        const out = n.eval(.{ in, 0, 1 / srate });
+        try ee(.{ in, in - 1 }, out);
+    }
+}
